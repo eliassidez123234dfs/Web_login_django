14e31eb83d08f9c76007f78a404306c8a2275599
Elias <hurtadoelias025@gmail.com>
2026-06-26 10:27:56 -0500

    style: redesign landing with services/process/cta sections, split-screen auth, improved footer

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
+    path('', views.catalogue, name='catalogue'),
+    path('admin/', views.admin_panel, name='admin_panel'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 
-# Create your views here.
+
+def catalogue(request):
+    return render(request, 'products/catalogue.html')
+
+
+@staff_member_required
+def admin_panel(request):
+    return render(request, 'products/admin_panel.html')
