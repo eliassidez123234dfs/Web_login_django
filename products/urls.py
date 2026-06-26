f260a7d192fbb811de79bb84124b43def7c0d3ad
Elias <hurtadoelias025@gmail.com>
2026-06-26 11:11:14 -0500

    feat: add product catalogue grid with card component and detail view

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.catalogue, name='catalogue'),
+    path('<int:pk>/', views.detail, name='detail'),
     path('admin/', views.admin_panel, name='admin_panel'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
+from .models import Producto
 
 
 def catalogue(request):
-    return render(request, 'products/catalogue.html')
+    productos = Producto.objects.filter(disponible=True)
+    return render(request, 'products/catalogue.html', {'productos': productos})
+
+
+def detail(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'products/detail.html', {'producto': producto})
 
 
 @staff_member_required
